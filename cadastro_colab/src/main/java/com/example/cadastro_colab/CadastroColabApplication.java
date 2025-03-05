239afe8697c0c2d2d2181cf29a27eab52668265f
Taiza Langa <taizalanga123@gmail.com>
2025-03-05 10:19:37 -0300

    Initial commit

---
+++ cadastro_colab/src/main/java/com/example/cadastro_colab/CadastroColabApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadastro_colab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroColabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroColabApplication.class, args);
+	}
+
+}
---
+++ cadastro_colab/src/test/java/com/example/cadastro_colab/CadastroColabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastro_colab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroColabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
